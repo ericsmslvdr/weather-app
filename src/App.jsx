70084595049ee363056b6435da98eb5ce567ff31
Eric Samuel Salvador <ericsmsalvador@gmail.com>
2023-12-08 20:32:02 +0800

    add searchbar component

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,11 @@ import { useEffect, useState } from 'react'
 import { fetchCurrentWeather } from './services/api'
 import Footer from './components/Footer'
 import WeatherCard from './components/WeatherCard'
+import SearchBar from './components/SearchBar'
 
 function App() {
   const [location, setLocation] = useState("")
   const [data, setData] = useState(null)
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const input = document.getElementById('location-input').value
-    setLocation(input)
-  }
 
   useEffect(() => {
     const fetchData = async () => {
@@ -33,27 +28,7 @@ function App() {
       <div className='container mx-auto flex justify-center h-screen'>
         <div className='m-auto w-1/2'>
           <div className='flex flex-col gap-4'>
-            <form>
-              <label for="location-input" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-              <div class="relative">
-                <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                  <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                  </svg>
-                </div>
-                <input
-                  type="search"
-                  id="location-input"
-                  class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="Search City here..."
-                  required />
-                <button
-                  type="submit"
-                  onClick={handleSubmit}
-                  class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >Search</button>
-              </div>
-            </form>
+            <SearchBar setLocation={setLocation} />
             {data == null
               ? null
               : <WeatherCard data={data} />
