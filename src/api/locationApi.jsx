eaca0f9532ec24adeb8b665f08e35fd763028aae
ericsmslvdr <ericsmsalvador@gmail.com>
2024-07-05 20:53:31 +0800

    refactor: Use https

--- src/api/locationApi.jsx
+++ src/api/locationApi.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // pls be gud, gh pages wont let me add keys to .env
 const key = "94fb58f8fac846bd90552644230410"
-const url = "http://api.weatherapi.com/v1"
+const url = "https://api.weatherapi.com/v1"
 
 export const fetchCurrentWeather = async (location) => {
     const API_ENDPOINT = `${url}/current.json?key=${key}&q=${location}`;
